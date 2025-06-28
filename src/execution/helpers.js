bc6d31d3205020a6333f219d66931ddbd5588547
Mathieu Decaffmeyer <5883963+mathieuprog@users.noreply.github.com>
2025-06-28 13:21:14 +0800

    Add mapNonNullish helper

--- src/execution/helpers.js
+++ src/execution/helpers.js
@@ -1,5 +1,13 @@
 import RootObject from '../document/RootObject';
 
+export function mapNonNullish(transformer) {
+  if (typeof transformer !== 'function') {
+    throw new Error('mapNonNullish(transformer) should receive a function as argument');
+  }
+
+  return (value) => value == null ? value : transformer(value);
+}
+
 export function throwIfNotInstanceOfDocument(instance) {
   if (!instance.operationType) {
     if (instance instanceof RootObject) {
